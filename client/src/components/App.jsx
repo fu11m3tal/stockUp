4f3d70f5fcd028e37afae71b3549ebe47b092478
Steven Yoh <syoh@Stevens-MacBook-Pro-2.local>
2020-06-29 14:24:46 -0700

    Implemented APEXCHARTS API for rending of charts for historical stock prices.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 // import { runInThisContext } from 'vm';
+import Line from './Line.jsx';
+
 import NewsCard from './NewsCard.jsx';
 import TOKEN from '../../../certification/config.js';
 class App extends React.Component {
@@ -30,16 +32,18 @@ class App extends React.Component {
           weburl: "https://www.apple.com/",
         },
       },
-      news: []
+      news: [],
+      priceTarget: {},
     }
     this.handle_search_input_change = this.handle_search_input_change.bind(this);
     this.handle_search_button_click = this.handle_search_button_click.bind(this);
     this.getNews = this.getNews.bind(this);
+    this.getPriceTarget = this.getPriceTarget.bind(this);
   }
 
   handle_search_input_change(e) {
     const search_input = e.target.value;
-    console.log(TOKEN);
+    // console.log(TOKEN);
     this.setState({search_input})
   }
 
@@ -47,30 +51,54 @@ class App extends React.Component {
     const { search_input } = this.state;
     axios.get(`https://finnhub.io/api/v1/stock/profile2?symbol=${search_input }&token=${TOKEN}`)
       .then(response => {
-        console.log(response.data)
+        // console.log(response.data)
+      })
+      .catch(err => {
+        console.log(err);
       })
   }
 
   getNews() {
     axios.get('https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2020-04-30&to=2020-05-01&token=brt2jjnrh5rd6rsr6mag')
-    .then(response => {
-      const news = response.data;
-      // console.log(response.data[0])
-      this.setState({news});
-    });
+      .then(response => {
+        const news = response.data;
+        // console.log(response.data[0])
+        this.setState({news});
+      })
+      .then(response => {
+        // console.log(this.state)
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
+
+  getPriceTarget() {
+
+    axios.get('https://finnhub.io/api/v1/stock/earnings?symbol=AAPL&token=brt2jjnrh5rd6rsr6mag')
+      .then(response => {
+        const priceTarget = response.data;
+        this.setState({priceTarget})
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
 
   componentDidMount() {
     this.getNews();
+    this.getPriceTarget();
   }
 
   render() { 
-    var {search_input, news} = this.state;
+    var {search_input, news, priceTarget} = this.state;
     return (
       <div>
         <h1>{this.state.title}</h1>
         <input id="search_input" value={search_input} onChange={this.handle_search_input_change}></input>
         <button id="search_button" onClick={this.handle_search_button_click}>Search</button>
+        <Line data={priceTarget} />
         {news.map((news, index) => (
           <NewsCard key={index} news={news} />
         ))}
--- client/src/components/Line.jsx
+++ client/src/components/Line.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import ApexCharts from 'apexcharts'
 
-const Line = ({  }) => {
+const Line = ({ data }) => {
+  var actuals = [];
+  var estimate = [];
+  var period = [];
+  var symbol = ""
+  for(var i = 0; i < data.length; i++) {
+    actuals.push(data[i].actual)
+    estimate.push(data[i].estimate)
+    period.push(data[i].period)
+    symbol = data[i].symbol
+  }
   var options = {
     chart: {
       type: 'line'
     },
+    stroke: {
+      curve: 'smooth',
+    },
     series: [{
-      name: 'sales',
-      data: [30,40,35,50,49,60,70,91,125]
+      name: symbol,
+      data: actuals,
     }],
     xaxis: {
-      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-    }
+      categories: period
+    },
   }
   
-  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  var chart = new ApexCharts(document.getElementById("chart"), options);
   chart.render()
   return(
     <div id="chart"></div>
